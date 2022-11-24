c210c442088eea8817cee152903172f08f9dab5d
yurochkina.elena <transnab@mail.ru>
2022-11-24 20:12:32 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 27. Напишите программу, которая принимает на вход число и выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine(Getmultiply(9));
+int Getmultiply(int num)
+{
+    int result = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
